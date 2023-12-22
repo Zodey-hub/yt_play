f80eb5d18b77ddcc2bfce95f211b0af6f3ea805f
Zodey <balint.mate.2006@gmail.com>
2023-12-22 19:29:22 +0100

    gui first commit

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,73 @@
-mod input_and_output;
-mod string_utils;
+use fltk::{
+    app,
+    button::Button,
+    frame::Frame,
+    prelude::{GroupExt, WidgetBase, WidgetExt},
+    window::Window, group::Flex,
+};
+
 mod video_utils;
 
-use std::process::Command;
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    FromPC,
+    ToPC,
+    Start,
+}
 
 fn main() {
-    let video_id = input_and_output::get_searched_video_id();
+    const PLAY: &str = "Play";
+    let app = app::App::default();
+    let screen_size = app::screen_size();
 
-    Command::new("vlc")
-        .arg("-I rc")
-        .arg(video_utils::get_video_direct_link(&video_id))
-        .arg("--no-video")
-        .arg("--play-and-exit")
-        .spawn()
-        .expect("Failed to start VLC! Maybe it's not in the path...");
+    let mut window = Window::new(
+        0,
+        0,
+        screen_size.0 as i32,
+        screen_size.1 as i32,
+        "yt_play by Zodey",
+    );
+    let frame = Frame::default().with_size(screen_size.0 as i32, screen_size.1 as i32).center_of(&window);
+    let mut flex = Flex::default().with_size(120, 140).center_of_parent().column();
+    let mut song_1_button = Button::new(100, 50, 200, 50, PLAY);
+    let mut song_2_button = Button::new(100, 150, 200, 50, PLAY);
+    let mut song_3_button = Button::new(100, 250, 200, 50, PLAY);
+    let mut song_4_button = Button::new(100, 350, 200, 50, PLAY);
+    let mut song_5_button = Button::new(100, 150, 200, 50, PLAY);
+
+    window.make_resizable(true);
+    window.end();
+    window.show();
+
+    let (s, r) = app::channel::<Message>();
+
+    song_1_button.emit(s, Message::FromPC);
+    song_2_button.emit(s, Message::ToPC);
+    song_3_button.emit(s, Message::Start);
+
+    while app.wait() {
+        if let Some(msg) = r.recv() {
+            match msg {
+                Message::FromPC => {
+                    println!("{:#?}", msg);
+                }
+                Message::ToPC => {
+                    println!("{:#?}", msg);
+                }
+                Message::Start => {
+                    /* println!(
+                        "{:?}",
+                        video_utils::get_videos(&video_utils::get_content(
+                            "https://www.youtube.com/results?search_query=stellar+ashes"
+                        ))[0]
+                            .thumbnail
+                    ); */
+
+                    for suggestion in video_utils::suggest_yt_queries("olyan ő") {
+                        println!("{}", suggestion);
+                    }
+                }
+            }
+        }
+    }
 }
--- src/video_utils.rs
+++ src/video_utils.rs
@@ -1,30 +1,46 @@
-use crate::string_utils;
 use std::process::Command;
-use tabled::Tabled;
 
-#[derive(Tabled, Clone)]
-#[allow(non_snake_case)]
+use http_req::{request::Request, uri::Uri};
+use regex::Regex;
+
 pub struct Video {
-    pub Index: usize,
-    pub Title: String,
-    pub Uploader: String,
-    pub Length: String,
-    pub View: String,
-    pub Uploaded: String,
-    pub VideoID: String,
+    pub index: usize,
+    pub title: String,
+    pub uploader: String,
+    pub length: String,
+    pub view: String,
+    pub uploaded: String,
+    pub video_id: String,
+    pub thumbnail: Vec<u8>,
 }
 
 impl Video {
     fn new() -> Self {
         Self {
-            Index: 0,
-            Title: String::new(),
-            Uploader: String::new(),
-            Length: String::new(),
-            View: String::new(),
-            Uploaded: String::new(),
-            VideoID: String::new(),
+            index: 0,
+            title: String::new(),
+            uploader: String::new(),
+            length: String::new(),
+            view: String::new(),
+            uploaded: String::new(),
+            video_id: String::new(),
+            thumbnail: Vec::new(),
         }
+    }
+
+    fn bestaudio_direct_link(&self) -> String {
+        let output = Command::new("yt-dlp")
+            .arg(format!("https://youtu.be/{}", self.video_id))
+            .arg("-f")
+            .arg("bestaudio")
+            .arg("-g")
+            .output()
+            .expect("Failed to start yt-dlp! Maybe it's not in the path...");
+        String::from_utf8_lossy(&output.stdout).trim().to_string()
+    }
+
+    fn play_best_audio(&self) {
+        Command::new("mpv").arg(self.bestaudio_direct_link());
     }
 }
 
@@ -49,56 +65,87 @@ pub fn get_videos(mut webpage_source: &str) -> [Video; 5] {
         end = webpage_source[start..].find(AFTER).unwrap() + 1;
         all_video_data = webpage_source[start..start + end].to_string();
 
-        videos[i].Index = i;
-        videos[i].Title = string_utils::give_text_between(&all_video_data, "{\"label\":\"", " by");
-        videos[i].Uploader =
-            string_utils::give_text_between(&all_video_data, "{\"runs\":[{\"text\":\"", "\"");
-        videos[i].Length =
-            string_utils::give_text_between(&all_video_data, "\"}},\"simpleText\":\"", "\"");
-        videos[i].View = string_utils::give_text_between(
-            &all_video_data,
-            "viewCountText\":{\"simpleText\":\"",
-            " ",
-        );
+        videos[i].index = i;
+        videos[i].title = give_text_between(&all_video_data, "{\"label\":\"", " by");
+        videos[i].uploader = give_text_between(&all_video_data, "{\"runs\":[{\"text\":\"", "\"");
+        videos[i].length = give_text_between(&all_video_data, "\"}},\"simpleText\":\"", "\"");
+        videos[i].view =
+            give_text_between(&all_video_data, "viewCountText\":{\"simpleText\":\"", " ");
 
         if all_video_data.contains("\"publishedTimeText\":{\"simpleText\":\"") {
             // if the video is uploaded by yt, it doesn't have date
-            videos[i].Uploaded = string_utils::give_text_between(
+            videos[i].uploaded = give_text_between(
                 &all_video_data,
                 "\"publishedTimeText\":{\"simpleText\":\"",
                 "\"",
             );
         } else {
-            videos[i].Uploaded = "YouTube".to_string();
+            videos[i].uploaded = "YouTube".to_string();
         }
-        videos[i].VideoID = string_utils::give_text_between(&all_video_data, "videoId\":\"", "\"");
+        videos[i].video_id = give_text_between(&all_video_data, "videoId\":\"", "\"");
+
+        http_req::request::get(
+            format!(
+                "https://i.ytimg.com/vi/{}/maxresdefault.jpg",
+                videos[i].video_id
+            ),
+            &mut videos[i].thumbnail,
+        )
+        .unwrap();
+
         webpage_source = &webpage_source[start + end..];
     }
     videos
 }
 
-/*pub fn get_video_direct_link(id: &str) -> String {
-    let mut api_response_buffer = Vec::new();
-    request::get(
-        format!(
-            "https://youtube-dl-web.vercel.app/api/info?q={}&f=bestaudio",
-            //id
-        ),
-        &mut api_response_buffer,
-    )
-    .unwrap();
-    let parsed = json::parse(&String::from_utf8_lossy(&api_response_buffer))
-        .expect("Failed to parse JSON response!");
-    parsed["url"].as_str().unwrap().trim().to_string()
-}*/
+fn give_text_between(source: &str, before: &str, after: &str) -> String {
+    let mut start_bytes = source.find(before).unwrap();
+    start_bytes += before.len();
+    let end_bytes = source[start_bytes..].find(after).unwrap();
+    source[start_bytes..start_bytes + end_bytes]
+        .trim()
+        .to_string()
+}
 
-pub fn get_video_direct_link(id: &str) -> String {
-    let output = Command::new("yt-dlp")
-        .arg(format!("https://youtu.be/{}", id))
-        .arg("-f")
-        .arg("bestaudio")
-        .arg("-g")
-        .output()
-        .expect("Failed to start yt-dlp! Maybe it's not in the path...");
-    String::from_utf8_lossy(&output.stdout).trim().to_string()
+pub fn get_content(url: &str) -> String {
+    let mut webpage_buffer = Vec::new();
+    let uri = Uri::try_from(url).unwrap();
+
+    let _response = Request::new(&uri)
+        .header("Accept-Language", "en-US")
+        .send(&mut webpage_buffer)
+        .unwrap();
+    String::from_utf8_lossy(&webpage_buffer).to_string()
+}
+
+pub fn suggest_yt_queries(input: &str) -> Vec<String> {
+    let mut webpage_buffer = Vec::new();
+    {
+        // get unparsed queries
+        let url = format!(
+            "https://suggestqueries-clients6.youtube.com/complete/search?client=youtube&ds=yt&q={}",
+            input.replace(" ", "+")
+        );
+        let uri = Uri::try_from(url.as_str()).unwrap();
+        let _response = Request::new(&uri)
+            .header(
+                "User-Agent",
+                "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0",
+            )
+            .send(&mut webpage_buffer)
+            .unwrap();
+    }
+
+    let re = Regex::new(r#""(.*?)""#).unwrap();
+    let webpage_text = String::from_utf8_lossy(&webpage_buffer);
+
+    let mut queries: Vec<String> = re
+        .captures_iter(&webpage_text)
+        .skip(1) // Skip the first element
+        .take(webpage_text.matches('"').count() - 3) // Take all but the last three elements
+        .map(|cap| String::from(&cap[1]))
+        .collect();
+
+    queries.truncate(queries.len() - 3);
+    queries
 }

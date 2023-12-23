db3574743f1850c49e632a43d11dbb5765b5d172
Zodey <balint.mate.2006@gmail.com>
2023-12-23 12:30:38 +0100

    mpv instead of vlc; making the program panic free; refactoring

--- src/input_and_output.rs
+++ src/input_and_output.rs
@@ -1,18 +1,16 @@
-use std::io;
-
 use http_req::{request::Request, uri::Uri};
+
 use std::convert::TryFrom;
+use std::{error::Error, io};
+
 use tabled::Table;
+use yt_play::{get_videos, Video};
 
-use crate::video_utils;
-
-pub fn get_searched_video_id() -> String {
+pub fn get_searched_video() -> Result<Video, Box<dyn Error>> {
     print!("Search for: ");
-    io::Write::flush(&mut io::stdout()).expect("Failed to flush!");
+    io::Write::flush(&mut io::stdout())?;
     let mut input = String::new();
-    std::io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line!");
+    std::io::stdin().read_line(&mut input)?;
 
     let input = input.trim().replace(' ', "+");
 
@@ -22,35 +20,32 @@ pub fn get_searched_video_id() -> String {
     );
 
     let mut webpage_buffer = Vec::new();
-    let uri = Uri::try_from(search_url.as_str()).unwrap();
+    let uri = Uri::try_from(search_url.as_str())?;
 
-    let _response = Request::new(&uri)
+    Request::new(&uri)
         .header("Accept-Language", "en-US")
-        .send(&mut webpage_buffer)
-        .unwrap();
+        .send(&mut webpage_buffer)?;
 
-    let videos = video_utils::get_videos(&String::from_utf8_lossy(&webpage_buffer));
+    let videos = get_videos(&String::from_utf8(webpage_buffer)?)?;
 
-    println!("{}", Table::new(videos.clone()));
+    println!("{}", Table::new(&videos));
 
     loop {
         println!("Please select the music by it's index!");
         let mut input = String::new();
 
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
+        io::stdin().read_line(&mut input)?;
 
         match input.trim().parse::<usize>() {
             Ok(number) => {
                 if number > 4 {
-                    println!("That's not a valid number. Please try again.");
+                    eprintln!("That is not a valid number. Please try again.");
                     continue;
                 }
-                return String::from(&videos[number].VideoID);
+                return Ok(videos[number].clone());
             }
             Err(_) => {
-                println!("That's not a valid number. Please try again.");
+                eprintln!("That is not a valid number. Please try again.");
                 continue;
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
 mod input_and_output;
-mod string_utils;
-mod video_utils;
 
-use std::process::Command;
+use std::error::Error;
 
-fn main() {
-    let video_id = input_and_output::get_searched_video_id();
-
-    Command::new("vlc")
-        .arg("-I rc")
-        .arg(video_utils::get_video_direct_link(&video_id))
-        .arg("--no-video")
-        .arg("--play-and-exit")
-        .spawn()
-        .expect("Failed to start VLC! Maybe it's not in the path...");
+fn main() -> Result<(), Box<dyn Error>> {
+    let video = input_and_output::get_searched_video()?;
+    video.play_best_audio()?;
+    Ok(())
 }
